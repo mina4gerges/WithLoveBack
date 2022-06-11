63196aa22778fa236d363e8d29d1b0f21ded509d
mina gerges <minagerges4@gmail.com>
2022-06-11 22:58:04 +0300

    Hello With Love backend

---
+++ src/main/java/com/withlove/withlove/WithLoveApplication.java
@@ -0,0 +1,13 @@
+package com.withlove.withlove;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WithLoveApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WithLoveApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/withlove/withlove/WithLoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.withlove.withlove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithLoveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
